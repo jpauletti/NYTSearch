10be4bcfb9afadd5f77ce7e6e7e44ec21563deda
Julia Pauletti <julia.pauletti@yahoo.com>
2019-04-06 14:08:44 -0500

    sync with front end

--- app.js
+++ app.js
@@ -1,37 +1,56 @@
+var $resultsDiv= $("#results-main");
+
+
 var begDateURL = "";
 var endDateURL = "";
 
-var searchTerm = "election";
-var begYear = 2012;
-var endYear = 2014;
-var numOfResults = 5;
 
-begDateURL = "&begin_date=" + begYear + "0101";
-endDateURL = "&end_date=" + endYear + "0101";
+$('button[type="submit"]').on("click", function(event) {
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&facet_fields=source&facet=true" + begDateURL + endDateURL + "&page=1&api-key=I2gR0YwA1mWt0O2QXE7Ah9FNQ14fhccl";
+    console.log(queryURL);
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    response.response.docs.splice(10, );
-    var array = response.response.docs.slice(0, 10);
-    console.log("array: " + array);
+    event.preventDefault();
 
-    console.log(response);
-    console.log(array[0].headline.main); // title
-    console.log(array[0].web_url); // link to article    
-    console.log(array[0].pub_date); // publish date 
+    var searchTerm = $("#search-input").val();
+    var begYear = $("#start-year").val();
+    var endYear = $("#end-year").val();
+    var numOfResults = $("#exampleFormControlSelect1").val();
+
+    if (begYear !== "" && endYear !== "") {
+        begDateURL = "&begin_date=" + begYear + "0101";
+        endDateURL = "&end_date=" + endYear + "0101";
+    }
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&facet_fields=source&facet=true" + begDateURL + endDateURL + "&page=1&api-key=I2gR0YwA1mWt0O2QXE7Ah9FNQ14fhccl";
 
 
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        $("#results-main").empty();
+        var array = response.response.docs.slice(0, numOfResults);
+        console.log("array: " + array);
+        console.log(numOfResults);
+
+        $.each(array, function(index, value) {
+            console.log(response);
+            console.log(array[index].headline.main); // title
+            console.log(array[index].web_url); // link to article    
+            console.log(array[index].pub_date); // publish date 
+
+            var newDiv = $("<div>");
+            var title = $("<h3>").text(array[index].headline.main);
+            var date = $("<p>").text(array[index].pub_date);
+            var link = $("<a>").attr("href", array[index].web_url).text(array[index].web_url);
+
+            newDiv.append(title, date, link);
+
+            $resultsDiv.append(newDiv);
+        })
 
 
+    });
 
-
-var myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
-var removed = myFish.splice(3, 1);
-
-// removed is ["mandarin"]
-// myFish is ["angel", "clown", "drum", "sturgeon"]
+})
